d5a8c968532ee0ff2b08446b097613c0222a8d8f
kuwala <kuwala@gmail.com>
2015-12-14 06:03:30 +0000

    added on-off switch on GPIO 4

--- buttons_gpio.py
+++ buttons_gpio.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+from subprocess import call
 from time import sleep
 from OSC import OSCClient, OSCMessage
 import RPi.GPIO as GPIO
@@ -18,6 +19,7 @@ BUT4 = 13
 BUT5 = 16
 BUT6 = 19
 
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUT1, GPIO.IN)
 GPIO.setup(BUT2, GPIO.IN)
@@ -25,6 +27,20 @@ GPIO.setup(BUT3, GPIO.IN)
 GPIO.setup(BUT4, GPIO.IN)
 GPIO.setup(BUT5, GPIO.IN)
 GPIO.setup(BUT6, GPIO.IN)
+
+# Adding Buttons 2
+BUT7  = 26
+BUT8  = 20
+BUT9  = 21
+BUT10 = 7
+GPIO.setup(BUT7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUT8, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUT9, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUT10, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Shutdown Button
+BUT11 = 4
+GPIO.setup(BUT11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Debounceing
 # - can add 0.1uF Cap across switch
@@ -50,6 +66,20 @@ GPIO.add_event_detect(BUT3, GPIO.BOTH, callback=threadCallBack, bouncetime=DEBOU
 GPIO.add_event_detect(BUT4, GPIO.BOTH, callback=threadCallBack, bouncetime=DEBOUNCE)
 GPIO.add_event_detect(BUT5, GPIO.BOTH, callback=threadCallBack, bouncetime=DEBOUNCE)
 GPIO.add_event_detect(BUT6, GPIO.BOTH, callback=threadCallBack, bouncetime=DEBOUNCE)
+
+GPIO.add_event_detect(BUT7, GPIO.BOTH, callback=threadCallBack, bouncetime=DEBOUNCE)
+GPIO.add_event_detect(BUT8, GPIO.BOTH, callback=threadCallBack, bouncetime=DEBOUNCE)
+GPIO.add_event_detect(BUT9, GPIO.BOTH, callback=threadCallBack, bouncetime=DEBOUNCE)
+GPIO.add_event_detect(BUT10, GPIO.BOTH, callback=threadCallBack, bouncetime=DEBOUNCE)
+
+# Shutdown event
+def shutdownFuncTest(channel):
+  print "offswitch detected"
+def shutdownFunc(channel):
+  call(["shutdown", "-h", "now"])
+GPIO.add_event_detect(BUT11, GPIO.FALLING, callback=shutdownFunc, bouncetime=DEBOUNCE)
+
+
 
 def sendOSCMessage(pin, value):
   # LOW/0 = pressed and HIGH/1 = released
@@ -84,4 +114,3 @@ except KeyboardInterrupt:
 
 GPIO.cleanup
 print "clean up on normal exit"
-
